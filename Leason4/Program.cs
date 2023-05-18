af94820f875d6ed3374aa05dcd76a497722f7ff2
Konstantin <kostik-kostik-12@mail.ru>
2023-05-18 22:06:15 +0300

    add Leason4

---
+++ Leason4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая
+//выводит массив из 8 элементов, заполненный
+//нулями и единицами в случайном порядке.
+
+void Initializated(int[] a)
+{
+for( int i = 0; i < a.Length; i++)
+{
+    a[i] = new Random().Next(0,2);
+}
+}
+
+void Write(int[] a)
+{
+    for( int i = 0; i < a.Length; i++)
+{
+    Console.Write(a[i] + " ");
+}
+}
+int[] a={1, 1, 1, 1, 1, 1, 1, 1};
+Initializated(a);
+Write(a);
